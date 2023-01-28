09213f7ef977178dda998af8c3b8f858d75d7e4c
tqfx <tqfx@foxmail.com>
2023-01-28 17:22:16 +0800

    2023-01-28 17:22:16
     cmake/TargetSupportsRelocatable.cmake | 13 +++++--------
     1 file changed, 5 insertions(+), 8 deletions(-)

--- cmake/TargetSupportsRelocatable.cmake
+++ cmake/TargetSupportsRelocatable.cmake
@@ -1,4 +1,5 @@
 function(TARGET_SUPPORTS_RELOCATABLE)
+  set(CMAKETMP ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp)
   get_cmake_property(ENABLED_LANGUAGES ENABLED_LANGUAGES)
   foreach(lang C ${ENABLED_LANGUAGES})
     if(CMAKE_${lang}_COMPILER)
@@ -9,15 +10,11 @@ function(TARGET_SUPPORTS_RELOCATABLE)
   if(NOT CMAKE_COMPILER)
     return()
   endif()
-  file(WRITE ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/object.c "\n")
-  execute_process(COMMAND ${CMAKE_COMPILER} -o object.o -r object.c
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp
-    ERROR_QUIET OUTPUT_QUIET RESULT_VARIABLE result
+  file(WRITE ${CMAKETMP}/object.c "\n")
+  execute_process(COMMAND ${CMAKE_COMPILER} -r object.c -o object.o
+    WORKING_DIRECTORY ${CMAKETMP} ERROR_QUIET OUTPUT_QUIET RESULT_VARIABLE result
   )
-  file(REMOVE
-    ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/object.o
-    ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/object.c
-  )
+  file(REMOVE ${CMAKETMP}/object.c ${CMAKETMP}/object.o)
   if(${result} EQUAL 0)
     set(TARGET_SUPPORTS_RELOCATABLE 1 CACHE INTERNAL "")
   else()
